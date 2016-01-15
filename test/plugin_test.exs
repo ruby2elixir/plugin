1406e92b2cf4ea2124527262efd42aa758880068
Roman Heinrich <roman.heinrich@gmail.com>
2016-01-15 18:55:00 +0100

    first tests

--- test/plugin_test.exs
+++ test/plugin_test.exs
@@ -1,78 +1,31 @@
 defmodule PluginTest do
-  use ExUnit.Case
+  use ExSpec, async: true
   doctest Plugin
 
-  defmodule PluginWithCustomOptions do
-    use Plugin.Builder
-    plug :reverse
+  # we start with really stupid tests
+  describe "single plug" do
+    defmodule TestPlugin do
+      use Plugin.Builder
+      plug :test1, []
 
-    def init(opts) do
-      opts
-    end
-
-    def call(acc, opts) do
-      super(acc, opts)
-      reverse(acc, opts)
-    end
-
-    def reverse(acc, opts) do
-      IO.inspect acc
-      {:cont, (acc |> String.reverse)}
-    end
-  end
-
-
-  # test
-
-  defmodule MyApp do
-    use Plugin.Builder
-    plug :hello, upper: true
-    plug :reversing_function
-    #plug PluginWithCustomOptions
-
-    def hello(value, opts) do
-      if opts[:upper],
-        do:   {:cont,"WORLD" },
-        else: {:cont,"world" }
-    end
-
-    def reversing_function(value, _opts) do
-      IO.inspect value
-      {:cont, (value |> String.reverse)}
-    end
-  end
-
-  test "the truth" do
-    assert 1 + 1 == 2
-  end
-
-  test "plugin works" do
-    # a = Plugin.call(MyApp, "some")
-    # IO.puts "********* "
-    # IO.inspect(a)
-  end
-
-
-  ###### another test
-
-  defmodule TestPlugin do
-    use Plugin.Builder
-    plug :test1, []
-    def test1(acc, _) do
-      if acc[:test1] do
-        {:stop, :test1}
-      else
-        {:cont, :test2}
+      def test1(acc, _) do
+        if acc[:test1] do
+          halt(acc)
+        else
+          acc
+        end
       end
     end
+
+    it "returns halted acc for test1" do
+      assert %{halted: true} = Plugin.call(TestPlugin, %{test1: true})
+
+    end
+
+    it "returns acc otherwise" do
+      a =  Plugin.call(TestPlugin, %{test2: true})
+      refute Map.get(a, :halted)
+      assert Map.get(a, :test2)
+    end
   end
-
-
-  test "test plugin" do
-    #a = Plugin.call(TestNextPlugin, %{test2: true}) # |> IO.inspect
-    #Plugin.call(TestNextPlugin, %{test1: true})
-    TestPlugin.call(%{test1: true}, []) |> IO.inspect
-
-  end
-
 end
