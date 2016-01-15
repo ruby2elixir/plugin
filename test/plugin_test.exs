091b0c0a037380b9598573ee8959602d6ec43784
Roman Heinrich <roman.heinrich@gmail.com>
2016-01-15 20:43:20 +0100

    some simple tests

--- test/plugin_test.exs
+++ test/plugin_test.exs
@@ -3,8 +3,8 @@ defmodule PluginTest do
   doctest Plugin
 
   # we start with really stupid tests
-  describe "single plug" do
-    defmodule TestPlugin do
+  describe "single plugin" do
+    defmodule SinglePlugin do
       use Plugin.Builder
       plug :test1, []
 
@@ -18,14 +18,59 @@ defmodule PluginTest do
     end
 
     it "returns halted acc for test1" do
-      assert %{halted: true} = Plugin.call(TestPlugin, %{test1: true})
-
+      assert %{halted: true} = Plugin.call(SinglePlugin, %{test1: true})
     end
 
     it "returns acc otherwise" do
-      a =  Plugin.call(TestPlugin, %{test2: true})
+      a =  Plugin.call(SinglePlugin, %{test2: true})
       refute Map.get(a, :halted)
       assert Map.get(a, :test2)
+    end
+  end
+
+
+  describe "single plugin multiple plug statements" do
+    defmodule SinglePluginMultiStatements do
+      use Plugin.Builder
+      plug :test1, []
+      plug :test2, []
+
+      def test1(acc, _) do
+        if acc[:test1] do
+          halt(acc)
+        else
+          acc
+        end
+      end
+
+      def test2(acc, _) do
+        acc = Map.put(acc, :test3, "added" )
+        halt(acc)
+      end
+    end
+
+    it "returns halted acc for test2" do
+      acc = Plugin.call(SinglePluginMultiStatements, %{test2: true})
+      assert %{halted: true}   = acc
+      assert %{test3: "added"} = acc
+    end
+  end
+
+
+  describe "requires a map as returned value" do
+    defmodule NoMapReturn do
+      use Plugin.Builder
+      plug :test1, []
+
+      def test1(acc, _) do
+        "invalid string"
+      end
+    end
+
+    it "raises" do
+      assert_raise RuntimeError, "expected test1/2 to return a Map", fn ->
+        Plugin.call(NoMapReturn, %{test2: true})
+      end
     end
   end
 end
