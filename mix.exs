89bcf01c72e2128409cea42ceb1eaea8ea9480f6
Roman Heinrich <roman.heinrich@gmail.com>
2016-01-15 18:03:47 +0100

    include ex_spec

--- mix.exs
+++ mix.exs
@@ -3,8 +3,8 @@ defmodule Plugin.Mixfile do
 
   def project do
     [app: :plugin,
-     version: "0.0.1",
-     elixir: "~> 1.2-dev",
+     version: "0.1.0",
+     elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps]
